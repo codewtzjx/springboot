3a2a95848cd2d79cecbf2bdd36682594ada0bd09
codewtzjx <wtzjx5201314pp>
2019-11-05 19:14:41 +0800

    first commit

---
+++ src/main/java/com/example/oristand/OristandApplication.java
@@ -0,0 +1,13 @@
+package com.example.oristand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OristandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OristandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oristand/OristandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oristand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OristandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
