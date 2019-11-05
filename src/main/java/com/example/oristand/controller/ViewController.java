8267ed121c11d2eebb0588bbf4325c1b2679462b
codewtzjx <wtzjx5201314pp>
2019-11-05 19:37:13 +0800

    first commit

---
+++ src/main/java/com/example/oristand/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.example.oristand.controller;
+
+/**
+ * Copyright 2019 Oriental Standard All rights reserved.
+ *
+ * @Author: WuTieZhu
+ * @ClassName ViewController
+ * @CreateTime 2019/11/5 19:35
+ * @Description: TODO
+ */
+public class ViewController {
+}
