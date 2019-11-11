9ac6579033d0a4b5fc4663d7bcc69b1395405349
codewtzjx <2870898907@qq.com>
2019-11-11 22:00:52 +0800

    201911112159

---
+++ src/main/java/com/domain/po/SystemSetting.java
@@ -0,0 +1,21 @@
+package com.domain.po;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="test")
+@PropertySource(value = "classpath:/application.yml")
+public class SystemSetting {
+    private String uploadPath;
+
+    public String getUploadPath() {
+        return uploadPath;
+    }
+
+    public void setUploadPath(String uploadPath) {
+        this.uploadPath = uploadPath;
+    }
+
+}
