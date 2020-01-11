63cc54973664faad4f8007145eb51af07e36869e
codewtzjx <wtzjx5201314pp>
2020-01-11 18:11:17 +0800

    dev commit

--- src/main/java/com/controller/AllTestController.java
+++ src/main/java/com/controller/AllTestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Description: TODO
  */
 @Controller
-@RequestMapping("/testwtz")
+@RequestMapping("/testjx")
 public class AllTestController {
 
     @Autowired
