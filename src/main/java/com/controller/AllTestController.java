e4c7779d4f6f39378e7895e0972cda1a4fa1e549
codewtzjx <wtzjx5201314pp>
2020-01-14 20:24:35 +0800

    master commit

--- src/main/java/com/controller/AllTestController.java
+++ src/main/java/com/controller/AllTestController.java
@@ -27,6 +27,7 @@ public class AllTestController {
     @ResponseBody
     public TbSysUser testSpringBoot() {
         TbSysUser tbSysUser = sysUserMapper.selectByPrimaryKey(3);
+        System.out.println("222");
         return tbSysUser;
     }
 }
