a209622a44331da9e8746a39e871e99f42989eaa
codewtzjx <wtzjx5201314pp>
2020-01-17 16:01:43 +0800

    master commit

---
+++ src/main/java/com/controller/test.java
@@ -0,0 +1,34 @@
+package com.controller;
+
+import com.domain.mapper.TbSysUserMapper;
+import com.domain.po.TbSysUser;
+import com.domain.po.TbSysUserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Copyright 2019 Oriental Standard All rights reserved.
+ *
+ * @Author: WuTieZhu
+ * @ClassName AllTestController
+ * @CreateTime 2019/11/5 19:35
+ * @Description: TODO
+ */
+@Controller
+@RequestMapping("/testjx")
+public class AllTestController {
+
+    @Autowired
+    private TbSysUserMapper sysUserMapper;
+
+    @RequestMapping("/test2")
+    @ResponseBody
+    public TbSysUser testSpringBoot() {
+        TbSysUser tbSysUser = sysUserMapper.selectByPrimaryKey(3);
+        System.out.println("111");
+        System.out.println("222");
+        return tbSysUser;
+    }
+}
