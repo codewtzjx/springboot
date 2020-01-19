f2dfaf88cd3876dff335aa8c63c55986905dc136
codewtzjx <wtzjx5201314pp>
2020-01-19 10:09:40 +0800

    wutiezhu

--- src/main/java/com/controller/Test3.java
+++ src/main/java/com/controller/Test3.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Description: TODO
  */
 @Controller
-@RequestMapping("/testjx")
+@RequestMapping("/wutiezhu")
 public class AllTestController {
 
     @Autowired
